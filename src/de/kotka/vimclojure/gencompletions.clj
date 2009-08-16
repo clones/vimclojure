7452d50bef86459d0e1e9e8df057567516f4c743
HonestHacker <>
2009-08-16 09:59:56 -0500

    Rename VimClojureBox to VimClojure and update paths for merge with VimClojure

--- src/de/kotka/vimclojure/gencompletions.clj
+++ src/de/kotka/vimclojure/gencompletions.clj
@@ -24,7 +24,6 @@
 
 (defn -main
   [nspace]
-  (require (symbol nspace))
   (let [completions (keys (ns-publics (symbol nspace)))]
     (with-out-file (str nspace "-keys.txt")
       (doseq [x (sort completions)]
